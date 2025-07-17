2183a35498e31fe803d75ad150166ed4caa18cc8
Tivyadharshini <tivyatharshini.j2023aiml@sece.ac.in>
2025-07-17 13:00:22 +0530

    Initial Commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard  = ({props}) => {
+  return (
+    <div>
+    <div>Dashboard page</div>
+    <h1>{props.email}</h1>
+    </div>
+  )
+}
+
+export default Dashboard 
--- app/page.js
+++ app/page.js
@@ -1,95 +1,35 @@
-import Image from "next/image";
+"use client";
+
+import Link from 'next/link';
 import styles from "./page.module.css";
+import {signIn,signOut,useSession} from 'next-auth/react';
+
+
 
 export default function Home() {
+
+  const {data: session}=useSession();
+  console.log(session);
+  if(session){
+        return(
+          <>
+          <p>You are signed in as {session.user.email}</p>
+          <Link href='/dashboard' email={session.user.email}>Dashboard</Link>
+          <button onClick={()=>{signOut()}}>SignOut</button>
+          </>
+        )
+      }
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <p>Please ! SignIn</p>
+      <button onClick={()=>{signIn()}}>SignIn</button>
+      <Link href='/home'>home</Link>
+      {/* <img src={session.user.image} alt="userImage"></img> */}
     </div>
   );
 }
+
+
+
+ 
